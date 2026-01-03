65b83887dfcca393997208cc794ead005d14d4e7
Austin Miller <austin.miller@cablesandkits.com>
2026-01-02 23:50:11 -0500

    wip

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function updateContentOpacity() {
-    const scrollPos = this.window.scrollY;
-    const content = this.document.getElementById('content');
-    // const chevron = this.document.getElementById('chevron');
-    const windowHeight = this.window.innerHeight;
+  const scrollPos = this.window.scrollY;
+  const content = this.document.getElementById('content');
+  // const chevron = this.document.getElementById('chevron');
+  const windowHeight = this.window.innerHeight;
 
-    const opacity = (scrollPos / windowHeight) * 4;
-    content.style.opacity = Math.min(1, opacity);
-    // chevron.style.visibility = "hidden";
+  const opacity = (scrollPos / windowHeight) * 5;
+  content.style.opacity = Math.min(1, opacity);
+  // chevron.style.visibility = "hidden";
 }
 
 window.addEventListener('scroll', updateContentOpacity);
